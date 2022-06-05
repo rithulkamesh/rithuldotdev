5b137680f590468b2aa0660b88d19e81904652ff
Rithul Kamesh <hi@rithul.dev>
2022-06-05 15:48:29 +0530

    Discord Redirect

--- next.config.js
+++ next.config.js
@@ -2,5 +2,14 @@ module.exports = {
   reactStrictMode: true,
   images: {
     domains: ['cdn.hashnode.com']
-  }
+  },
+  async redirects() {
+    return [
+      {
+        source: '/discord',
+        destination: 'https://discord.gg/tqtuSttKxA',
+        permanent: true,
+      },
+    ]
+  },
 };
